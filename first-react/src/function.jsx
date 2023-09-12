6b7f71aa9280894cb05075be6bca5e262fd17150
IshratJahanRintu <ishrat2415@student.nstu.edu.bd>
2023-09-12 18:08:26 +0600

    firts

---
+++ first-react/src/function.jsx
@@ -0,0 +1,10 @@
+import React from "react" ;
+
+
+export default function Component(){
+    return (
+       <><h1>This is a heading for functional component</h1>
+        <p>This is a paragraph from functional component</p>
+       </>
+    );
+}
---
+++ first-react/src/test.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import ReactDOM from "react-dom/client";
+
+class ClassComponent extends React.Component{
+
+   render() {
+        return (
+            <div >
+                <h1>My class heading </h1>
+                <p>first paragraph from class component </p>
+            </div>
+        );
+    }
+
+
+}
+
+export default  ClassComponent;
